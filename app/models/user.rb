321e2dffd8879b0e381bee66a32fadab5540ab42
Paulo André <pauloacmelo@gmail.com>
2014-04-13 02:14:10 -0300

    Make user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :microposts
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :password, length: { minimum: 6 }
+
+  before_save { self.email = email.downcase }
 end
